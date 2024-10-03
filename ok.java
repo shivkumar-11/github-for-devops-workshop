53a62a8aea961f980a1045cb222e6075c8ae1a0e
unknown <shivkumar952595@gmail.com>
2024-10-04 01:08:17 +0530

    check kr bhai

---
+++ ok.java
@@ -0,0 +1,5 @@
+public class  ok{
+    public static void main(String[] args){
+        System.out.println("kaise hai aap log");
+    }
+}
---
+++ okk.java
@@ -0,0 +1,6 @@
+public class okk {
+    public static void main(String[] args) {
+        System.out.println("kya hal hai");
+    }
+    
+}
