d23c78625814d82d11bf00b0ba9d5a95b52beb74
shivkumar-11 <shivkumar952595@gmail.com>
2024-10-04 03:05:45 +0530

    Update okk.java

--- okk.java
+++ okk.java
@@ -1,6 +1,7 @@
 public class okk {
     public static void main(String[] args) {
         System.out.println("kya hal hai");
+      //  https://github.com/shivkumar-11/flutter_projects.git
     }
     
 }
